336d156f9bee83e0f3e1182c981b18b1c4f0ac76
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-02-06 22:42:44 -0500

    .

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -40,16 +40,12 @@
 		<!-- NAVBAR SHRINKING -->
 
 		<script type="text/javascript">
-			// NAVBAR SHRINKING
-
-			jQuery(function($) {
-				$(window).scroll(function() {
-				  if ($(document).scrollTop() > 50) {
-				    $('nav').addClass('shrink');
-				  } else {
-				    $('nav').removeClass('shrink');
-				  }
-				});
+			$(window).scroll(function() {
+			  if ($(document).scrollTop() > 50) {
+			    $('nav').addClass('shrink');
+			  } else {
+			    $('nav').removeClass('shrink');
+			  }
 			});
 		</script>
 		
