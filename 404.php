ec476c0b76f032ff81235cafee511a041e72da08
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-02-11 13:15:04 -0500

    .

--- 404.php
+++ 404.php
@@ -15,7 +15,7 @@
 	<div class="row-fluid">
 		<div id="content" class="col-sm-8 col-sm-offset-2">
 
-			<h2>You got lost, it's ok</h2>
+			<h2 class="text-center">You got lost, it's ok</h2>
 
 			<div class="text-center">
 				<div style="max-width: 500px; margin: 0 auto; display: inline;" id="_giphy_10FGpRQr33JBKw"></div><script>var _giphy = _giphy || []; _giphy.push({id: "10FGpRQr33JBKw",w: 500, h: 282});var g = document.createElement("script"); g.type = "text/javascript"; g.async = true;g.src = ("https:" == document.location.protocol ? "https://" : "http://") + "giphy.com/static/js/widgets/embed.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);</script>
