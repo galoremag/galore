22dafc074d6775ec9185a76c6a379287ab102d46
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-04-01 09:09:51 -0400

    .

--- single-pop.php
+++ single-pop.php
@@ -14,54 +14,52 @@
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div id="content" class="col-sm-8 col-sm-offset-2">
-			<div class="pop-in">
 
-				<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 
-				<article>
-					<div id="social-links">
-						<ul id="post-social" class="post-social">
-							<li><a href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
-							<li><a href="#" target="popup" onclick="window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
-						</ul>
-					</div>
-					<h2><?php the_title(); ?></h2>
-
-					<p class="byline"><i class="fa fa-star"></i> <time datetime="<?php the_time( 'Y-m-d' ); ?>">Posted on <?php the_time('M j, Y \@\ g:i a'); ?></time> <i class="pink fa fa-flash"></i> By <?php the_author_posts_link(); ?></p>
-					
-					<div class="spacer20"></div>
-
-					<?php the_content(); ?>			
-
-					<?php if ( get_the_author_meta( 'description' ) ) : ?>
-					<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
-					<h3>About <?php echo get_the_author() ; ?></h3>
-					<?php the_author_meta( 'description' ); ?>
-					<?php endif; ?>
-					<ul class="hidden-sm single-social">
-						<li><a href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i> &nbsp;<h4 class="nomarg">Share on Facebook</h4></a></li>
-						<li><h4 class="social-title"><?php the_title(); ?></h4></li>
+			<article>
+				<div id="social-links">
+					<ul id="post-social" class="post-social">
+						<li><a href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="#" target="popup" onclick="window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
 					</ul>
-					<?php comments_template( '', true ); ?>
+				</div>
+				<h2><?php the_title(); ?></h2>
 
-				</article>
-				<?php endwhile; ?>
+				<p class="byline"><i class="fa fa-star"></i> <time datetime="<?php the_time( 'Y-m-d' ); ?>">Posted on <?php the_time('M j, Y \@\ g:i a'); ?></time> <i class="pink fa fa-flash"></i> By <?php the_author_posts_link(); ?></p>
+				
+				<div class="spacer20"></div>
 
-				<hr>
-				<h2 class="text-center">Gimme <span>More</span> Pop <span><i class="fa fa-flash"></i></span></h2>
+				<?php the_content(); ?>			
 
-				<ul id="related-posts" class="row-fluid">
-					<?php $post_ids = array(); $loop = new WP_Query( array( 'posts_per_page' => 4, 'orderby' => 'rand' ) ); ?>
-
-				    <?php
-						if($post_ids){
-							//Implode the posts and set a variable to pass to our exclude param.
-							$postsNotIn = implode(",", $post_ids);
-						}
-						echo do_shortcode('[ajax_load_more orderby="rand" category="pop" exclude="'.$postsNotIn.'" button_label="More Shit"]');
-				    ?>
+				<?php if ( get_the_author_meta( 'description' ) ) : ?>
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
+				<h3>About <?php echo get_the_author() ; ?></h3>
+				<?php the_author_meta( 'description' ); ?>
+				<?php endif; ?>
+				<ul class="hidden-sm single-social">
+					<li><a href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i> &nbsp;<h4 class="nomarg">Share on Facebook</h4></a></li>
+					<li><h4 class="social-title"><?php the_title(); ?></h4></li>
 				</ul>
-			</div>
+				<?php comments_template( '', true ); ?>
+
+			</article>
+			<?php endwhile; ?>
+
+			<hr>
+			<h2 class="text-center">Gimme <span>More</span> Pop <span><i class="fa fa-flash"></i></span></h2>
+
+			<ul id="related-posts" class="row-fluid">
+				<?php $post_ids = array(); $loop = new WP_Query( array( 'posts_per_page' => 4, 'orderby' => 'rand' ) ); ?>
+
+			    <?php
+					if($post_ids){
+						//Implode the posts and set a variable to pass to our exclude param.
+						$postsNotIn = implode(",", $post_ids);
+					}
+					echo do_shortcode('[ajax_load_more orderby="rand" category="pop" exclude="'.$postsNotIn.'" button_label="More Shit"]');
+			    ?>
+			</ul>
 		</div>
 	</div>
 </div>
