587f485ff3b89a1be82dd8dbe297fe254f59b7c1
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-05-13 17:21:08 -0400

    Pinterest Verification

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -9,6 +9,10 @@
 		<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	  	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=0"><!-- Remove if you're not building a responsive site. (But then why would you do such a thing?) -->
+
+		<!-- Pinterest Verification -->
+		<meta name="p:domain_verify" content="8c3f8aadea8dcff760f98a676ca9a2f7"/>
+
 		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 		<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/img/favicon.ico"/>
 
