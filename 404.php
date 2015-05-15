decbd3a90c2e635f884b5444ebafe388f55c00c6
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-05-15 18:33:13 -0400

    fix 404 page

--- 404.php
+++ 404.php
@@ -18,7 +18,13 @@
 			<h2 class="text-center">You got lost, it's ok</h2>
 
 			<div class="text-center">
-				<div style="max-width: 500px; margin: 0 auto; display: inline;" id="_giphy_10FGpRQr33JBKw"></div><script>var _giphy = _giphy || []; _giphy.push({id: "10FGpRQr33JBKw",w: 500, h: 282});var g = document.createElement("script"); g.type = "text/javascript"; g.async = true;g.src = ("https:" == document.location.protocol ? "https://" : "http://") + "giphy.com/static/js/widgets/embed.js";var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(g, s);</script>
+				<div id="_giphy_tv"></div>
+				<script>
+				var _giphy_tv_tag="sexy";
+				var g = document.createElement('script'); g.type = 'text/javascript'; g.async = true;
+				g.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'giphy.com/static/js/widgets/tv.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(g, s);
+				</script>
 			</div>
 
 			<div class="spacer40"></div>
