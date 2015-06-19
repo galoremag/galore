58f4a750493d494bb928cd0fefeeb12d4d47abed
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-06-19 13:15:07 -0400

    better padding on sidebar

--- category-editorial.php
+++ category-editorial.php
@@ -14,12 +14,12 @@
 <div class="container-fluid nopad">
 	<div class="row-fluid">
 		<div class="col-md-8 col-md-offset-2 container-fixed nopad">
-		<?php echo do_shortcode( '[new_royalslider id="6"]' ); ?>
+		<?php echo do_shortcode( '[new_royalslider id="8"]' ); ?>
 		</div>
 	</div>
 	<div class="row-fluid">
 		<div id="content" class="col-md-8 col-sm-12">
-			<h2 class="text-center">The Latest in <span><?php echo single_cat_title( '', false ); ?></span> <i class="fa fa-file"></i></h2>
+			<h2 class="text-center">The Latest in <span><?php echo single_cat_title( '', false ); ?></span> <i class="fa fa-diamond"></i></h2>
 			<hr>
 			<ol>
 			<?php query_posts('category_name=editorial&posts_per_page=4&offset=4&ignore_sticky_posts=4'); ?>
@@ -64,7 +64,7 @@
 			<div class="spacer40"></div>
 		</div>
 		<div id="sidebar-anchor"></div>
-		<div id="sidebar" class="sidebar col-md-4 pad40 hidden-sm">
+		<div id="sidebar" class="sidebar col-md-4 hidden-sm">
 			<h2>Trending</h2>
 			<?php
 			    $args = array(
