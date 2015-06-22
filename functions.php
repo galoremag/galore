291fe30f9f5d71c07936a0f53ba6ce3b35aa14a4
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-06-22 13:31:14 -0400

    shop removal, style tweaks

--- functions.php
+++ functions.php
@@ -289,6 +289,10 @@
 	    wp_redirect( 'http://tv.galoremag.com', 301 ); exit;
 	}
 
+	if (strpos($url,'shop/') !== false) {
+	    wp_redirect( 'http://galoremag.com', 301 ); exit;
+	}
+
 	// Popular Stories
 
 	// function to display number of posts.
