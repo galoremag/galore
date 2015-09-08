cdfc0099e1f6f01aa0d00514076e8558795a8e0b
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-08 03:32:17 -0400

    html req for add to cart ;

--- shopp/category.php
+++ shopp/category.php
@@ -33,7 +33,7 @@
 							<?php endif; ?>
 						</a>
 					</div>
-					<?php shopp('product','addtocart', 'ajax=on&class=btn btn-block btn-large btn-bp verMarg10'); ?>
+					<?php shopp('product','addtocart', 'ajax=html&class=btn btn-block btn-large btn-bp verMarg10'); ?>
 				</div><!--/span3-->
 			<?php endwhile; ?>
 
