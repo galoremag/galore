8de446d19a0faa77a3de12e50c2234a2629642db
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-08 03:28:23 -0400

    add to cart AJAX?

--- shopp/category.php
+++ shopp/category.php
@@ -33,7 +33,7 @@
 							<?php endif; ?>
 						</a>
 					</div>
-					<?php shopp('product','addtocart', 'class=btn btn-block btn-large btn-bp'); ?>
+					<?php shopp('product','addtocart', 'ajax=on&class=btn btn-block btn-large btn-bp verMarg10'); ?>
 				</div><!--/span3-->
 			<?php endwhile; ?>
 
