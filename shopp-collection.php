65c8203801f0ac5b262e0edf3a2b2c9531a89e11
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-08 03:20:09 -0400

    shop cat tempplate

---
+++ shopp-collection.php
@@ -0,0 +1,47 @@
+<?php
+ /*
+ Template Name: Shop
+
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * Please see /external/starkers-utilities.php for info on Starkers_Utilities::get_template_parts()
+ *
+ * @package 	WordPress
+ * @subpackage 	Starkers
+ * @since 		Starkers 4.0
+ */
+?>
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
+
+<div class="container-fluid">
+	<div class="row-fluid">
+		<div id="content" class="col-sm-10 col-sm-offset-1">
+			<?php if ( have_posts() ): ?>
+			<h2 class="text-center"><i class="fa fa-moon-o"></i> Shop <i class="fa fa-moon-o"></i></h2>
+			<hr>
+
+			<?php if (have_posts()) : while ( have_posts() ) : the_post(); ?>
+
+			<?php the_content();?>
+			<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'twentyten' ), 'after' => '' ) ); ?>
+
+			<?php endwhile; endif; ?>
+			<?php else: ?>
+			<h2>No posts to display in <?php echo single_cat_title( '', false ); ?></h2>
+			<?php endif; ?>
+
+			<!-- <div id="post-nav">
+				<ul>
+					<li id="page-left" class="alignleft pull-left"><?php previous_posts_link( '<h4><i class="fa fa-chevron-left"></i> &nbsp; PREV</h4>' ); ?></li>
+					<li id="page-right" class="alignright pull-right"><?php next_posts_link( '<h4>NEXT &nbsp; <i class="fa fa-chevron-right"></i></h4>', '' ); ?></li>
+				</ul>
+			</div> -->
+			<div class="spacer40"></div>
+		</div>
+	</div>
+</div>
+
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
