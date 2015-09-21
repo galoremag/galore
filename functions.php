08e9a8a7e5dc41e93bf8638129e43a6b89c1a0a4
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-21 13:54:57 -0400

    add additional forward slash for /TV/ redirect

--- functions.php
+++ functions.php
@@ -298,7 +298,7 @@
 	    wp_redirect( 'http://tv.galoremag.com', 301 ); exit;
 	}
 
-	// if (strpos($url,'shop/') !== false) {
+	// if (strpos($url,'/shop/') !== false) {
 	//     wp_redirect( 'http://galoremag.com', 301 ); exit;
 	// }
 
