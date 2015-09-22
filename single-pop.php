0553968b3f1c23ce09123228bfc13809187b268f
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-22 09:58:44 -0400

    manual trigger of ga.push() on test element

--- single-pop.php
+++ single-pop.php
@@ -46,7 +46,7 @@
 				<?php endif; ?>
 				<div class="spacer20"></div>
 				<ul class="single-social">
-					<li><a href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i> &nbsp;<h4 class="nomarg">Share on Facebook</h4></a></li>
+					<li><a href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400'); dataLayer.push({'event': 'facebook-share'});"><i class="fa fa-facebook"></i> &nbsp;<h4 class="nomarg">Share on Facebook</h4></a></li>
 					<!-- <li class="pad10">
 						<div class="fb-like hidden-xs hidden-sm" data-href="http://galoremag.com" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
 					</li> -->
