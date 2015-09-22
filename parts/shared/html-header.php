b54aac72f6a73f161426d06dac821bab5d7d062f
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-09-22 14:24:34 -0400

    real visitor performance info on pingdom

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -114,6 +114,19 @@
 		    s.parentNode.insertBefore(p, s);
 		})();
 		</script>
+
+		<!-- Pingdom Real User Info -->
+		<script>
+		var _prum = [['id', '56019c8cabe53d87787f8ac3'],
+		             ['mark', 'firstbyte', (new Date()).getTime()]];
+		(function() {
+		    var s = document.getElementsByTagName('script')[0]
+		      , p = document.createElement('script');
+		    p.async = 'async';
+		    p.src = '//rum-static.pingdom.net/prum.min.js';
+		    s.parentNode.insertBefore(p, s);
+		})();
+		</script>
 		
 	</head>
 	<body>
