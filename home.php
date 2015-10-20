4cb2e0f110b6f6f0e66b8fdf84d271de51b2d250
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-10-20 19:13:17 -0400

    .

--- home.php
+++ home.php
@@ -45,7 +45,15 @@ $url = $thumb[0];
 
 		<div id="glides">
 			<?php
-				$postslist = get_posts('numberposts=20&order=DESC&orderby=date&offset=0&post_type=post&post__in=sticky_posts&ignore_sticky_posts=1');
+				$postslist = get_posts('numberposts=20&order=DESC&orderby=date&offset=0&post_type=post');
+				$postslist = get_posts( array(
+					'offset' => 0,
+					'numberposts' => 20,
+					'orderby' => 'date',
+					'order' => 'DESC',
+					'post__in'  => get_option( 'sticky_posts' ),
+					'ignore_sticky_posts' => 4
+				));
 				foreach ($postslist as $post) :
 				setup_postdata($post);
 
