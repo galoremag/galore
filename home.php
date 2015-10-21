9bdb1335f780d3e88015079a06b3cc9c782d8703
Aaron Smulktis <aaronsmulktis@gmail.com>
2015-10-21 17:41:11 -0400

    .

--- home.php
+++ home.php
@@ -47,7 +47,7 @@ $url = $thumb[0];
 
 				<?php
 
-				$postid = $wp_query->post->ID;
+				// $postid = $wp_query->post->ID;
 				$sticky = get_option( 'sticky_posts' );
 
 			    $args_ordinary = array(
@@ -79,7 +79,7 @@ $url = $thumb[0];
 			    
 					<div class="glide ad">
 						<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('thumbnail'); ?></a>
-						<div class="adFlag">Presented By <?php echo get_post_meta( $postid, 'sponsor', true ); ?></div>
+						<div class="adFlag">Presented By <?php echo get_post_meta( $post->ID, 'sponsor', true ); ?></div>
 						<a href="<?php the_permalink(); ?>"><h3><?php the_title(); ?></h3></a>
 						<!-- <h4>
 							<?php 
