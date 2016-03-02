d9b32ea4fc957f687c443fe7f5293d734860868a
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-03-02 15:16:41 -0500

    shifting more grid classes for home

--- home.php
+++ home.php
@@ -197,12 +197,12 @@
 		<div class="sex-dating-in">
 			<div class="container">
 				<div class="row-fluid">
-					<div class="col-md-10 col-md-offset-1">
+					<div class="col-sm-12">
 						<h2>Sex + Dating <i class="fa fa-heart"></i></h2>
 					</div>
 				</div>
 				<div class="row-fluid">
-					<div class="col-md-7 col-sm-8 col-md-offset-1">
+					<div class="col-sm-8">
 						<?php
 						$postslist = get_posts('category_name=sex-dating&numberposts=1&order=DESC&orderby=date');
 						foreach ($postslist as $post) :
@@ -223,7 +223,7 @@
 						</div>
 						<?php endforeach; ?>
 					</div>
-					<div class="col-md-3 col-sm-4">
+					<div class="col-sm-4">
 						<?php
 						$postslist = get_posts('category_name=sex-dating&numberposts=2&order=DESC&orderby=date&offset=1');
 						foreach ($postslist as $post) :
@@ -260,7 +260,7 @@
 		<div class="pop-in">
 			<div class="container">
 				<div class="row-fluid">
-					<div class="col-md-10 col-md-offset-1">
+					<div class="col-sm-12">
 						<h2>Guilty Pleasure <i class="fa fa-star"></i></h2>
 						<h3>Best of Pop Culture</h3>
 						<div class="spacer20"></div>
@@ -372,7 +372,7 @@
 
 	<div class="container nopad">
 		<div class="row-fluid">
-			<div id="content" class="col-md-10 col-md-offset-1">
+			<div id="content" class="col-sm-12">
 				<h2 id="stickHead" class="text-center"><i class="fa fa-diamond"></i> More Everything <i class="fa fa-diamond"></i></h2>
 				<hr>
 
