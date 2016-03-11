17363ae3f9c84e635dcf36e630ec91c0d046d986
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-03-11 17:35:11 -0500

    add TV to menu

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -23,14 +23,15 @@
 				</div>
 				<div>
 					<ul class="top-social navbar-right tab hidden-xs">
-						<li><a href="<?php bloginfo('url'); ?>/shop" title="Galore Shop">Shop</a></li>
-						<?php if(shopp('cart','hasitems')) : ?>
-							<li><a href="<?php shopp('cart','url'); ?>" title="Galore Shopping Cart"><span class="badge"><?php shopp('cart','totalitems'); ?></span> <i class="fa fa-shopping-cart"></i></a></li>
-						<?php endif ?>
+						<li><a href="http://tv.galoremag.com" title="GaloreTV">TV</a></li>
 						<li><a id="signupButton" href="#"><i class="fa fa-envelope"></i></a></li>
 						<li><a href="https://www.facebook.com/galoremag" target="_blank"><i class="fa fa-facebook"></i></a></li>
 						<li><a href="http://instagram.com/galore/" target="_blank"><i class="fa fa-instagram"></i></a></li>
 						<li><a href="https://twitter.com/thegaloremag" target="_blank"><i class="fa fa-twitter"></i></a></li>
+						<li><a href="<?php bloginfo('url'); ?>/shop" title="Galore Shop">Shop</a></li>
+						<?php if(shopp('cart','hasitems')) : ?>
+							<li><a href="<?php shopp('cart','url'); ?>" title="Galore Shopping Cart"><span class="badge"><?php shopp('cart','totalitems'); ?></span> <i class="fa fa-shopping-cart"></i></a></li>
+						<?php endif ?>
 						<!-- <li><a href="http://princeandjacob.tumblr.com/" target="_blank"><i class="fa fa-tumblr"></i></a></li> -->
 						<!-- <li><a href="https://www.youtube.com/channel/UCyzzsgpNlmLBKYcXLM3Ro3g" target="_blank"><i class="fa fa-youtube-play"></i></a></li> -->
 						<!-- <li class=""><a href="http://www.slashergirl.com/collections/shop-galore?ref=galore" target="_blank">SHOP</a></li> -->
@@ -48,8 +49,8 @@
 				</div>
 			</div> -->
 			<ul class="modal-nav-menu text-center">
-				<li><a href="<?php bloginfo('url'); ?>/category/style" title="Style">Style</a></li>
-				<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a></li>
+				<li>GaloreTV <span class="badge">New</span></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a> + <a href="<?php bloginfo('url'); ?>/category/style" title="Style">Style</a></li>
 				<li><a href="<?php bloginfo('url'); ?>/category/sex-dating" title="Sex + Dating">Sex + Dating</a></li>
 				<li><a href="<?php bloginfo('url'); ?>/category/health" title="health">Health</a></li>
 				<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a></li>
