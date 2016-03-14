05ff0bbecdebba2b6326ec3dd45ea9d6643806de
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-03-14 12:57:24 -0400

    fix TV link in menu

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -49,7 +49,7 @@
 				</div>
 			</div> -->
 			<ul class="modal-nav-menu text-center">
-				<li>GaloreTV <span class="badge">New</span></li>
+				<li><a href="http://tv.galoremag.com" title="GaloreTV">GaloreTV <span class="badge">New</span></a></li>
 				<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a> + <a href="<?php bloginfo('url'); ?>/category/style" title="Style">Style</a></li>
 				<li><a href="<?php bloginfo('url'); ?>/category/sex-dating" title="Sex + Dating">Sex + Dating</a></li>
 				<li><a href="<?php bloginfo('url'); ?>/category/health" title="health">Health</a></li>
@@ -92,7 +92,7 @@ $postslist = get_posts( $args );
 foreach ($postslist as $post) : setup_postdata($post);
 ?>
 
-<?php 
+<?php
 $thumb = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large');
 $url = $thumb[0];
 ?>
@@ -104,7 +104,7 @@ $url = $thumb[0];
 	<p class="specialTip">Scroll to site</p>
 	<img height='1' width='1' src='https://tracking.jetpackdigital.com/jpt?sid=1242&oid=5697&lid=28249&csid=&c=0&itt=EOTgSDNteBPGBZseExTPjt3KGpZXpe1WEEYEwE1Y%2FTII51KPC7NhewUsrcjb%2FwYB&ord=[RANDOM]'/>
 	<a href='https://tracking.jetpackdigital.com/jpc?sid=1242&oid=5697&lid=28249&csid=&c=0&ict=J4USQfJyvBlMu2SRh3iQGGbxFnwXyP0RFM%2BYPCbvTs%2F5ejjlVlrQ7BuFo%2B2HhxazyywI0bIkDPiwooLpuEJwqSs35ce8191aKY9dlUH0AMug7cTRufjy%2FQ%3D%3D&ord=[RANDOM]'/></a>
-	
+
 	<a id="scroll-down"><i class="fa fa-arrow-circle-o-down"></i></a>
 </div>
 
