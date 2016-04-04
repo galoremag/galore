c862ab36e0846639706008e2a7db68d851969877
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-04-04 16:02:25 -0400

    .

--- parts/shared/footer.php
+++ parts/shared/footer.php
@@ -9,7 +9,7 @@
 		</div>
 	</div>
 
-	<!-- Facebook Like Modal -->
+	<!-- Newsletter Signup -->
 	<div id="fb-modal">
 	    <div class="fb-modal-dialog col-md-4 col-md-offset-4">
 	        <div class="fb-modal-content">
@@ -18,10 +18,11 @@
 	            </div>
 	            <div class="fb-modal-body">
 	                <div class="brand-white" style="margin: 0 auto -100px auto;"></div>
-	                <h2 class="horPad20 text-center">Oh Snap.</h2>
-	                <div class="pad10 fb-like">
-										<img src="images/galore_snapcode.svg" title="Galore Snapchat - Snapcode" alt="Galore Snapchat - Snapcode" />
-									</div>
+	                <h1 class="horPad20 text-center">Do You Like?</h1>
+	                <p class="text-center">
+	                    Some things are only found on Facebook. Don't miss out.
+	                </p>
+	                <div class="pad10 fb-like" data-href="https://www.facebook.com/galoremag" data-layout="button_count" data-action="like" data-show-faces="true" data-share="false"></div>
 	            </div>
 	        </div>
 	    </div>
@@ -30,21 +31,21 @@
 
 	<!-- Snapchat Follow -->
 	<div id="snapchat-modal" class="modal" tabindex="-1" role="dialog" aria-labelledby="email-signup" aria-hidden="true">
-	    <div class="snapchat-modal-dialog col-md-4 col-md-offset-4">
-	        <div class="snapchat-modal-content">
-	            <div class="snapchat-modal-header">
-	                <a id="snapchatClose" class="close" data-dismiss="snapchat-modal" aria-label="Close"><span aria-hidden="true" class="fa fa-close"></span></a>
-	            </div>
-	            <div class="snapchat-modal-body">
-	                <div class="brand-white" style="margin: 0 auto -100px auto;"></div>
-	                <h2 class="horPad20 text-center">Oh Snap.</h2>
-	                <div class="pad10 snapchat-like">
-										<img src="images/galore_snapcode.svg" title="Galore Snapchat - Snapcode" alt="Galore Snapchat - Snapcode" />
+			<div class="snapchat-modal-dialog col-md-4 col-md-offset-4">
+					<div class="snapchat-modal-content">
+							<div class="snapchat-modal-header">
+									<a id="snapchatClose" class="close" data-dismiss="snapchat-modal" aria-label="Close"><span aria-hidden="true" class="fa fa-close"></span></a>
+							</div>
+							<div class="snapchat-modal-body">
+									<div class="brand-white" style="margin: 0 auto -100px auto;"></div>
+									<h2 class="horPad20 text-center">Oh Snap.</h2>
+									<div class="pad10 snapchat-like">
+										<img src="<?php echo content_url(); ?>/themes/galore/images/galore_snapcode.svg" title="Galore Snapchat - Snapcode" alt="Galore Snapchat - Snapcode" />
 									</div>
-	            </div>
-	        </div>
-	    </div>
-	    <div class="back"></div>
+							</div>
+					</div>
+			</div>
+			<div class="back"></div>
 	</div>
 
 	<!-- Newsletter Signup -->
