8d2b05d96b3a560ad2406b3db2c6fa683c99856a
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-06-15 14:44:39 -0400

    add comscore tag

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -155,6 +155,21 @@
 		nativeAI('create','9bd8568d-6ad3-4d6a-955a-0d8a7779328e',isTestMode);
 		</script>
 
+		<!-- Begin comScore Tag -->
+		<script>
+			var _comscore = _comscore || [];
+			_comscore.push({ c1: "2", c2: "22114619" });
+			(function() {
+			var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
+			s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
+			el.parentNode.insertBefore(s, el);
+			})();
+		</script>
+		<noscript>
+		 <img src="http://b.scorecardresearch.com/p?c1=2&c2=22114619&cv=2.0&cj=1" />
+		</noscript>
+		<!-- End comScore Tag -->
+
 	</head>
 	<body>
 
