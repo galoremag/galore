c4963c287a5f2caa3877c0006377168802a5c5cd
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-07-05 18:27:21 -0400

    attempt to remove PHP mobile logic

--- parts/shared/single-two-300x250.php
+++ parts/shared/single-two-300x250.php
@@ -1,19 +1,13 @@
-<?php if (wp_is_mobile()) : ?>
+<!-- /60899964/Article_Mobile_300x250_pos2 -->
+<div id='div-gpt-ad-1465835581876-4' style='height:250px; width:300px;'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-4'); });
+</script>
+</div>
 
-  <!-- /60899964/Article_Mobile_300x250_pos2 -->
-  <div id='div-gpt-ad-1465835581876-4' style='height:250px; width:300px;'>
-  <script type='text/javascript'>
-  googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-4'); });
-  </script>
-  </div>
-
-<?php else : ?>
-
-  <!-- /60899964/Article_300x250_970x250_pos2 -->
-  <div id='div-gpt-ad-1465835581876-1'>
-  <script type='text/javascript'>
-  googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-1'); });
-  </script>
-  </div>
-
-<?php endif; ?>
+<!-- /60899964/Article_300x250_970x250_pos2 -->
+<div id='div-gpt-ad-1465835581876-1'>
+<script type='text/javascript'>
+googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-1'); });
+</script>
+</div>
