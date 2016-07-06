53d8407919f1280c52541dce9f8a3b2f21ca4e9a
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-07-06 17:46:13 -0400

    remove PHP device detection on home page

--- home.php
+++ home.php
@@ -132,7 +132,6 @@ $url = $thumb[0];
 					<!-- 300x250 unit -->
 					<div class="thumbnail col-sm-6 horpad10">
 						<div class="nopad">
-							<?php if ( wp_is_mobile() ) : ?>
 
 								<!-- /60899964/Home_Mobile_300x250 -->
 								<div id='div-gpt-ad-1465835581876-13' style='height:250px; width:300px;'>
@@ -141,8 +140,6 @@ $url = $thumb[0];
 								</script>
 								</div>
 
-							<?php else: ?>
-
 								<!-- /60899964/Home_300x250 -->
 								<div id='div-gpt-ad-1465835581876-9' style='height:250px; width:300px;'>
 								<script type='text/javascript'>
@@ -150,7 +147,6 @@ $url = $thumb[0];
 								</script>
 								</div>
 
-							<?php endif; ?>
 						</div>
 					</div>
 
@@ -278,8 +274,6 @@ $url = $thumb[0];
 					</div>
 					<?php endforeach; ?>
 
-					<?php if (wp_is_mobile()) : ?>
-
 						<!-- /60899964/Home_300x250_970x250_pos2 -->
 						<div id='div-gpt-ad-1465835581876-10'>
 						<script type='text/javascript'>
@@ -287,16 +281,12 @@ $url = $thumb[0];
 						</script>
 						</div>
 
-					<?php else : ?>
-
 						<!-- /60899964/Home_Mobile_300x250_pos2 -->
 						<div id='div-gpt-ad-1465835581876-14' style='height:250px; width:300px;'>
 						<script type='text/javascript'>
 						googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-14'); });
 						</script>
 						</div>
-
-					<?php endif; ?>
 
 					<?php
 					$args = array( 'category_name' => 'health', 'post_type' => 'post', 'showposts' => 2, 'orderby' => 'date', 'order' => 'DESC', 'offset' => 1 );
@@ -321,7 +311,6 @@ $url = $thumb[0];
 	</div>
 
 	<!-- 100x100 -->
-	<?php if ( wp_is_mobile() ) : ?>
 
 		<!-- /60899964/Home_Mobile_OOP -->
 		<div id='div-gpt-ad-1465835581876-18'>
@@ -330,16 +319,12 @@ $url = $thumb[0];
 		</script>
 		</div>
 
-	<?php else: ?>
-
 		<!-- /60899964/Home_OOP -->
 		<div id='div-gpt-ad-1465835581876-19'>
 		<script type='text/javascript'>
 		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-19'); });
 		</script>
 		</div>
-
-	<?php endif; ?>
 
 	<!-- SEX + DATING SECTION -->
 
@@ -511,8 +496,6 @@ $url = $thumb[0];
 							endif;
 							?>
 
-							<?php if ( wp_is_mobile() ) : ?>
-
 								<!-- /60899964/Home_Mobile_300x250_pos3 -->
 								<div id='div-gpt-ad-1465835581876-15' style='height:250px; width:300px;'>
 								<script type='text/javascript'>
@@ -520,16 +503,12 @@ $url = $thumb[0];
 								</script>
 								</div>
 
-							<?php else: ?>
-
 								<!-- /60899964/Home_300x250_970x250_pos3 -->
 								<div id='div-gpt-ad-1465835581876-11'>
 								<script type='text/javascript'>
 								googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-11'); });
 								</script>
 								</div>
-
-							<?php endif; ?>
 
 						</div>
 					</div>
@@ -589,64 +568,6 @@ $url = $thumb[0];
 				<?php endforeach; ?>
 
 			  <?php wp_reset_postdata(); ?>
-
-				<!-- 3rd Unit -->
-
-				<hr />
-
-				<ul class="alm-listing alm-ajax">
-
-					<?php
-					$args = array( 'category_name' => 'beauty', 'post_type' => 'post', 'showposts' => 4, 'orderby' => 'date', 'order' => 'DESC' );
-
-					$postslist = get_posts( $args );
-
-					// $postslist = get_posts('tag=darling&numberposts=4&order=DESC&orderby=date&offset=0');
-					foreach ($postslist as $post) : setup_postdata($post);
-					?>
-					<li class="post">
-						<article class="row-fluid">
-							<div class="nopad col-sm-5">
-								<div class="catlinks"><?php the_category(); ?></div>
-								<div class="thumb">
-									<a href="<?php esc_url( the_permalink() ); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_post_thumbnail('medium'); ?></a>
-								</div>
-								<h4 class="hidden-xs">Share this post</h4>
-								<ul class="post-social pull-left hidden-xs">
-									<li><a class="share-email" href="mailto:Friend@somewhere.com?subject=<?php the_title(); ?>&body=<?php the_permalink(); ?>"><i class="fa fa-envelope"></i></a></li>
-									<li><a class="share-facebook" href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
-									<li><a class="share-twitter" href="#" target="popup" onclick="window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
-									<li class="visible-xs"><h4>Share This Post</h4></li>
-								</ul>
-							</div>
-							<div class="nopadright col-sm-7">
-								<h3 class="nomartop"><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
-								<p class="byline hidden-xs"><time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time('M j, Y \@\ g:i a'); ?></time> <i class="pink fa fa-flash"></i> <?php the_author_posts_link(); ?></p>
-								<p class="byline visible-xs pull-left nomarg"><time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time('M j, Y \@\ g:i a'); ?></time> <br /> <i class="pink fa fa-flash"></i> <?php the_author_posts_link(); ?></p>
-								<ul class="post-social visible-xs">
-									<li><a class="share-email" href="mailto:Friend@somewhere.com?subject=<?php the_title(); ?>&body=<?php the_permalink(); ?>"><i class="fa fa-envelope"></i></a></li>
-									<li><a class="share-facebook" href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
-									<li><a class="share-twitter" href="#" target="popup" onclick="window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
-								</ul>
-
-								<div class="excerpt hidden-xs"><?php the_excerpt(); ?></div>
-
-								<p class="pull-left hidden-xs"><a href="<?php esc_url( the_permalink() ); ?>">Full Story <i class="fa fa-mars"></i></a></p>
-
-							</div>
-						</article>
-					</li>
-					<?php endforeach; ?>
-
-				</ul>
-
-				<hr />
-
-				<!­­ cmnUNT | Begin ad tag ­­>
-				<div id="cmn_ad_tag_content" class="text-center">
-					<script type="text/javascript">cmnUNT('300x250', tile_num++);</script>
-				</div>
-				<!­­ cmnUNT | End ad tag ­­>
 
 				<hr />
 
