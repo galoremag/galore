f0b046fa9f155bd1a55044623db68888e68c7254
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-07-08 16:46:09 -0400

    remove infini-ads

--- parts/shared/single-interstitial.php
+++ parts/shared/single-interstitial.php
@@ -4,11 +4,3 @@
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-5'); });
 </script>
 </div>
-
-
-<!-- /60899964/Article_Interstitial -->
-<div id='div-gpt-ad-1465835581876-2' class="hidden-xs">
-<script type='text/javascript'>
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-2'); });
-</script>
-</div>
