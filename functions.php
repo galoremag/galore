93f929c0677e779e6817b75ff9286c44018068ca
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-08-03 14:45:37 -0400

    allow SVG uploads through media uploader in WP

--- functions.php
+++ functions.php
@@ -420,4 +420,11 @@
 	  }
 	}
 
+	// Allow SVG uploads
+	function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+	}
+	add_filter('upload_mimes', 'cc_mime_types');
+
 ?>
