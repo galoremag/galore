407b86c62b6b59dd1651e853f7a3fd011171b9a6
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-08-12 16:20:23 -0400

    tweak menu

--- parts/shared/header.php
+++ parts/shared/header.php
@@ -25,7 +25,7 @@
 				<div class="container-fluid">
 					<div class="navbar-header">
 						<div id="modal-nav-button" class="nav-button pull-left">
-							<div id="nav-button"><a href="#" class="hmbrgr" onClick="menuToggle()"></a></div>
+							<div id="nav-button"><a href="#" id="hmbrgr" class="hmbrgr"></a></div>
 						</div>
 						<div id="site-search" class="pull-left hidden-xs">
 							<!-- <?php get_search_form(); ?> -->
@@ -102,7 +102,7 @@
 				<div class="container-fluid">
 					<div class="navbar-header">
 						<div id="modal-nav-button" class="nav-button pull-left">
-							<div id="nav-button"><a href="#" class="hmbrgr" onClick="menuToggle()"></a></div>
+							<div id="nav-button"><a href="#" id="hmbrgr" class="hmbrgr"></a></div>
 						</div>
 						<div id="site-search" class="pull-left hidden-xs">
 							<!-- <?php get_search_form(); ?> -->
