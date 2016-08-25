b6bfb7cb27439c798fa0a3bb197ff09d39d24c64
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-08-25 16:27:00 -0400

    new ajax single loader

--- single-beauty.php
+++ single-beauty.php
@@ -11,45 +11,83 @@
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
-<div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+<div class="container-fluid nopad">
+	<div class="row-fluid">
+		<div id="content" class="col-md-8 col-sm-12 container-fixed">
+			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+			<article>
+				<?php setPostViews(get_the_ID()); ?>
+				<div class="single-featured-image">
+					<div class="catlinks"><?php the_category(); ?></div>
+					<?php the_post_thumbnail('large'); ?>
+				</div>
+				<h1><?php the_title(); ?></h1>
+				<div id="social-links">
+					<ul id="post-social" class="post-social hidden-xs hidden-sm">
+						<li><a class="share-facebook" href="javascript:;" target="popup" onclick="recordShare(); window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
+						<li><a class="share-twitter" href="javascript:;" target="popup" onclick="recordShare(); window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
+					</ul>
+				</div>
 
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
+				<p class="byline"><i class="fa fa-bomb"></i> <time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time('M j, Y \@\ g:i a'); ?></time> <i class="pink fa fa-flash"></i> By <?php the_author_posts_link(); ?></p>
+				<?php edit_post_link('EDIT. THIS. PIECE.', '<p>', '</p>'); ?>
+				<div class="spacer20"></div>
+				<?php the_content(); ?>
+				<div class="spacer20"></div>
+				<div class="author-info row hidden-xs">
+					<?php if ( get_the_author_meta( 'description' ) ) : ?>
+					<div class="author-bio col-md-8 col-md-offset-2 text-right">
+						<h3>About The Author: <span><?php echo get_the_author() ; ?></span></h3>
+						<?php the_author_meta( 'description' ); ?>
+					</div>
+					<div class="author-pic col-md-2">
+						<?php echo get_avatar( get_the_author_meta( 'user_email' ) ); ?>
+					</div>
+					<?php endif; ?>
+				</div>
+				<div class="spacer20"></div>
+				<!-- <ul class="single-social">
+					<li><a class="share-facebook" href="javascript:;" target="popup" onclick="recordShare(); window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i> &nbsp;<h4 class="nomarg">Share on Facebook</h4></a></li>
+					<li class="pull-right"><h4 class="social-title"><?php the_title(); ?></h4></li>
+				</ul> -->
+			</article>
+			<?php endwhile; ?>
+			<!-- <hr> -->
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
+			<!­­ cmnUNT | Begin ad tag ­­>
+			<div id="cmn_ad_tag_content" class="container-fluid nopad">
+				<script type="text/javascript">cmnUNT('100x100', tile_num++);</script>
+			</div>
+			<!­­ cmnUNT | End ad tag ­­>
 
-			if ( is_singular( 'attachment' ) ) {
-				// Parent post navigation.
-				the_post_navigation( array(
-					'prev_text' => _x( '<span class="meta-nav">Published in</span><span class="post-title">%title</span>', 'Parent post link', 'twentysixteen' ),
-				) );
-			} elseif ( is_singular( 'post' ) ) {
-				// Previous/next post navigation.
-				the_post_navigation( array(
-					'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Next post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-					'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentysixteen' ) . '</span> ' .
-						'<span class="screen-reader-text">' . __( 'Previous post:', 'twentysixteen' ) . '</span> ' .
-						'<span class="post-title">%title</span>',
-				) );
-			}
+			<h2 class="text-center">Gimme More <i class="fa fa-bomb"></i> <span>Beauty</span></h2>
+			<div class="spacer20"></div>
+			<hr />
+			<div class="spacer20"></div>
+			<ul id="related-posts" class="row-fluid">
 
-			// End of the loop.
-		endwhile;
-		?>
+				<!-- Special Post -->
 
-	</main><!-- .site-main -->
+				<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/sponsored-md' ) ); ?>
 
-	<?php get_sidebar( 'content-bottom' ); ?>
+				<!­­ cmnUNT | Begin ad tag ­­>
+				<div id="cmn_ad_tag_content" class="text-center">
+					<script type="text/javascript">cmnUNT('300x250', tile_num++);</script>
+				</div>
+				<!­­ cmnUNT | End ad tag ­­>
 
-</div><!-- .content-area -->
+				<hr />
 
+			    <?php
+			    	// if($post_ids){
+					// 	//Implode the posts and set a variable to pass to our exclude param.
+					// 	$postsNotIn = implode(",", $post_ids);
+					// }
+					echo do_shortcode('[ajax_load_more previous_post="true" previous_post_id="'.$wp_query->post->ID.'" post_type="post" button_label="Loading" category="beauty" repeater="template_1"]');
+					// echo do_shortcode('[ajax_load_more orderby="date" offset="4" category="beauty" exclude="'.$wp_query->post->ID.'" button_label="Loading"]');
+			    ?>
+		</div>
+
+	</div>
+</div>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
