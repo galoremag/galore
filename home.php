8bae6bbbfebc6dc170ea33b65910da27d54c515f
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-08-26 14:50:22 -0400

    get pos4 back in there

--- home.php
+++ home.php
@@ -544,14 +544,14 @@
 						<hr />
 
 						<!-- /60899964/Home_300x250_970x250_pos4 -->
-						<div id='div-gpt-ad-1467230007625-0' class="hidden-xs">
+						<div id='div-gpt-ad-1467230007625-0' class="hidden-xs text-center">
 						<script type='text/javascript'>
 						googletag.cmd.push(function() { googletag.display('div-gpt-ad-1467230007625-0'); });
 						</script>
 						</div>
 
 						<!-- /60899964/Home_Mobile_300x250_pos4 -->
-						<div id='div-gpt-ad-1467230007625-1' style='height:250px; width:300px;' class="visible-xs">
+						<div id='div-gpt-ad-1467230007625-1' style='height:250px; width:300px;' class="visible-xs text-center">
 						<script type='text/javascript'>
 						googletag.cmd.push(function() { googletag.display('div-gpt-ad-1467230007625-1'); });
 						</script>
