9de8b38be0d4a9aeccfda1e9f6ddaa464a195e51
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-08-30 10:52:13 -0400

    working nice

--- single-beauty.php
+++ single-beauty.php
@@ -13,7 +13,7 @@
 
 <div class="container-fluid nopad">
 	<div class="row-fluid">
-		<div id="content" class="col-md-8 col-sm-12">
+		<div id="content" class="col-md-8 col-sm-12 col-md-offset-2">
 			<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 			<article>
 				<?php setPostViews(get_the_ID()); ?>
@@ -60,12 +60,60 @@
 			</div>
 			<!­­ cmnUNT | End ad tag ­­>
 
-			<h2 class="text-center">Gimme More <i class="fa fa-bomb"></i> <span>Beauty</span></h2>
+			<h2 class="text-center">Gimme More <i class="fa fa-diamond"></i> <span>Beauty</span></h2>
 			<div class="spacer20"></div>
-			<hr />
-			<div class="spacer20"></div>
+			<ul id="related-posts" class="row-fluid">
 
 				<!-- Special Post -->
+
+				<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/sponsored-md' ) ); ?>
+
+				<!­­ cmnUNT | Begin ad tag ­­>
+				<div id="cmn_ad_tag_content" class="text-center">
+					<script type="text/javascript">cmnUNT('300x250', tile_num++);</script>
+				</div>
+				<!­­ cmnUNT | End ad tag ­­>
+
+				<hr />
+
+				<ul class="container-fluid">
+
+					<?php
+					global $wp_query;
+					$cat_ID = get_the_category($post->ID);
+					$cat_ID = $cat_ID[0]->cat_ID;
+					$this_post = $post->ID;
+
+					$args = array( 'category_name' => 'beauty', 'post_type' => 'post', 'showposts' => 3, 'orderby' => 'date', 'order' => 'DESC', 'post__not_in' => array($this_post) );
+
+					$postslist = get_posts( $args );
+
+					// $postslist = get_posts('tag=darling&numberposts=4&order=DESC&orderby=date&offset=0');
+					foreach ($postslist as $post) : setup_postdata($post);
+					?>
+					<li class="related post pull-left col-sm-4">
+						<div class="row-fluid">
+							<div class="nopad col-sm-12">
+								<div class="catlinks"><?php the_category(); ?></div>
+								<div class="thumb">
+									<a href="<?php esc_url( the_permalink() ); ?>" title="<?php the_title(); ?>" rel="bookmark"><?php the_post_thumbnail('medium'); ?></a>
+								</div>
+							</div>
+						</div>
+						<div class="row-fluid">
+							<div class="nopad col-sm-12">
+								<h3 class="nomartop"><a href="<?php esc_url( the_permalink() ); ?>" title="Permalink to <?php the_title(); ?>" rel="bookmark"><?php the_title(); ?></a></h3>
+								<p class="byline hidden-xs"><time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time('M j, Y \@\ g:i a'); ?></time> <i class="pink fa fa-flash"></i> <?php the_author_posts_link(); ?></p>
+								<p class="byline visible-xs pull-left nomarg"><time datetime="<?php the_time( 'Y-m-d' ); ?>"><?php the_time('M j, Y \@\ g:i a'); ?></time> <br /> <i class="pink fa fa-flash"></i> <?php the_author_posts_link(); ?></p>
+								<p class="pull-left hidden-xs"><a href="<?php esc_url( the_permalink() ); ?>">Full Story <i class="fa fa-mars"></i></a></p>
+							</div>
+						</div>
+					</li>
+					<?php endforeach; ?>
+
+				</ul>
+
+				<hr />
 
 				<!­­ cmnUNT | Begin ad tag ­­>
 				<div id="cmn_ad_tag_content" class="text-center">
@@ -80,9 +128,10 @@
 					// 	//Implode the posts and set a variable to pass to our exclude param.
 					// 	$postsNotIn = implode(",", $post_ids);
 					// }
-					echo do_shortcode('[ajax_load_more previous_post="true" previous_post_id="'.$wp_query->post->ID.'" exclude="'.$wp_query->post->ID.'" post_type="post" button_label="Loading" category="beauty" repeater="template_1"]');
-					// echo do_shortcode('[ajax_load_more orderby="date" offset="4" category="beauty" exclude="'.$wp_query->post->ID.'" button_label="Loading"]');
+
+					echo do_shortcode('[ajax_load_more offset="3" previous_post="true" previous_post_id="'.$wp_query->post->ID.'" orderby="date" category="beauty" exclude="'.$wp_query->post->ID.'" button_label="Loading" repeater="template_2" post_type="post"]');
 			    ?>
+			</ul>
 		</div>
 
 	</div>
