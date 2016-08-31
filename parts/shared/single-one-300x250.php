20df03a970b85c56404e34dda213d4594fac384e
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-08-31 13:00:49 -0400

    lil more cleanup

--- parts/shared/single-one-300x250.php
+++ parts/shared/single-one-300x250.php
@@ -1,12 +1,12 @@
 <!-- /60899964/Article_Mobile_300x250 -->
-<div id='div-gpt-ad-1465835581876-3' style='height:250px; width:300px;' class="visible-xs">
+<div id='div-gpt-ad-1465835581876-3' style='height:250px; width:300px;' class="visible-xs center-block">
 <script type='text/javascript'>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-3'); });
 </script>
 </div>
 
 <!-- /60899964/Article_300x250_970x250 -->
-<div id='div-gpt-ad-1465835581876-0' style='height:250px; width:300px;' class="hidden-xs">
+<div id='div-gpt-ad-1465835581876-0' style='height:250px; width:300px;' class="hidden-xs center-block">
 <script type='text/javascript'>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-0'); });
 </script>
--- parts/shared/single-two-300x250.php
+++ parts/shared/single-two-300x250.php
@@ -1,12 +1,12 @@
 <!-- /60899964/Article_Mobile_300x250_pos2 -->
-<div id='div-gpt-ad-1465835581876-4' style='height:250px; width:300px;' class="visible-xs">
+<div id='div-gpt-ad-1465835581876-4' style='height:250px; width:300px;' class="visible-xs center-block">
 <script type='text/javascript'>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-4'); });
 </script>
 </div>
 
 <!-- /60899964/Article_300x250_970x250_pos2 -->
-<div id='div-gpt-ad-1465835581876-1' style='height:250px; width:300px;' class="hidden-xs">
+<div id='div-gpt-ad-1465835581876-1' style='height:250px; width:300px;' class="hidden-xs center-block">
 <script type='text/javascript'>
 googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-1'); });
 </script>
