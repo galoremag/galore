3f35d588ed6ead2cbc7a0b6c54f8f65d9a3cbca7
Anthony Grullon <agrullon95@gmail.com>
2016-09-02 18:11:12 -0400

    Completed first draft of Contributor Submission Page

--- page-contribute.php
+++ page-contribute.php
@@ -19,13 +19,136 @@ Template Name: Contributor Submission
 
 <div class="container-fluid">
     <div class="row-fluid">
-        <div id="content" class="col-sm-10 col-sm-offset-1">
+        <div id="content" class="nopad col-sm-10 col-sm-offset-1">
 
-          <div class="row">
+          <div class="row-fluid contributorHeading">
             <div class="col-sm-12">
-              <h1>Write For Us</h1>
-
+              <h1 class="text-center" style="font-size: 70px; font-weight: bold; letter-spacing: 5px; color: #ffffff;">Write For Us</h1>
             </div>
+          </div>
+
+          <div class="row text-center contributorStats">
+            <div class="col-sm-12">
+              <h2>Contributing to the Voice of Generation-Y</h2>
+            </div>
+            <div class="col-sm-12">
+              <div class="col-sm-4">
+                <i class="fa fa-globe" aria-hidden="true"></i>
+                <p class="stats">Over 1M Unique Visitors</p>
+              </div>
+              <div class="col-sm-4">
+                <i class="fa fa-instagram" aria-hidden="true"></i>
+                <p class="stats">Over 200k Instagram Followers</p>
+              </div>
+              <div class="col-sm-4">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+                <p class="stats">Over 200k Twitter Followers</p>
+              </div>
+            </div>
+          </div>
+
+          <div class="row text-center contributorDescriptions">
+            <div class="col-sm-12">
+              <h2>What We Seek</h2>
+            </div>
+            <div class="col-sm-12">
+              <p><span style="color: #fa7470;">Galore’s </span> contributing writers are carefully selected based on a
+                combination of talent, experience, and passion. We leave no stone unturned, and every topic,
+                be it controversial or not, is explored through a variety of lenses and perspectives.</p>
+            </div>
+          </div>
+
+          <div class="row text-center contributorForm">
+            <form>
+            <div class="col-sm-12">
+              <h2>Apply</h2>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-4 col-form-label">First:</label>
+              <div class="col-xs-8">
+                <input class="form-control" type="text">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-4 col-form-label">Last:</label>
+              <div class="col-xs-8">
+                <input class="form-control" type="text">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-4 col-form-label">Email Address:</label>
+              <div class="col-xs-8">
+                <input class="form-control" type="email">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-4 col-form-label">Birthdate:</label>
+              <div class="col-xs-8">
+                <input class="form-control" type="date">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-4 col-form-label">Location:</label>
+              <div class="col-xs-8">
+                <input class="form-control" type="text">
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-12 col-form-label">Interests/Hobbies:</label>
+              <p>Tell us about yourself</p>
+              <div class="col-xs-12">
+                <textarea class="form-control" rows="5" type="text">
+                </textarea>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-12 col-form-label">Sample Writing Link:</label>
+              <p>Please provide a link to a sample of your writing online</p>
+              <div class="col-xs-12">
+                <input class="form-control" type="url" placeholder="https://galoremag.com/barbie-has-officially-become-an-instagram-thot/">
+              </div>
+            </div>
+            <div class="form-group row formInterests">
+              <label class="col-xs-12 col-form-label">Interested Verticals:</label>
+              <div class="checkbox" class="col-sm-12">
+                <label>
+                  <input type="checkbox" value="">
+                  Beauty
+                </label>
+                <label>
+                  <input type="checkbox" value="">
+                  Health
+                </label>
+                <label>
+                  <input type="checkbox" value="">
+                  Models
+                </label>
+              </div>
+              <div class="checkbox" class="col-sm-12">
+                <label>
+                  <input type="checkbox" value="">
+                  Parties
+                </label>
+                <label>
+                  <input type="checkbox" value="">
+                  Pop
+                </label>
+                <label>
+                  <input type="checkbox" value="">
+                  Sex + Dating
+                </label>
+              </div>
+            </div>
+            <div class="form-group row">
+              <label class="col-xs-12 col-form-label">Article Ideas:</label>
+              <p>Please list some article ideas you have</p>
+              <div class="col-xs-12">
+                <textarea class="form-control" rows="5" type="text">
+                </textarea>
+              </div>
+            </div>
+            <button type="submit" class="btn btn-default text-center">Submit</button>
+            </form>
           </div>
 
         </div>
