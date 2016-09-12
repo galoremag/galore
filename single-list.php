84d5456ee6af6fa0565b95ef77f96183ba684ba2
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-09-12 16:42:19 -0400

    wtf why are list items nesting inside other list items, attempt to fix

--- single-list.php
+++ single-list.php
@@ -22,23 +22,6 @@ WP Post Template: Fancy List
 			<article>
 				<?php setPostViews(get_the_ID()); ?>
 
-				<!-- <div id="listIntro" class="row-fluid">
-					<div class="container">
-						<div class="col-sm-8 col-sm-offset-2">
-
-							<?php edit_post_link('EDIT. THIS. PIECE.', '<p>', '</p>'); ?>
-
-							<h1><?php the_title(); ?></h1>
-							<?php the_content(); ?>
-							<div class="text-center">
-								<a class="btn btn-primary" onClick="$.scrollify.move('#1');">Read The List</a>
-							</div>
-						</div>
-					</div>
-				</div>
-
-				<div class="spacer20"></div> -->
-
 				<!-- Item on List -->
 
 				<?php if( have_rows('items') ): ?>
@@ -60,9 +43,6 @@ WP Post Template: Fancy List
 										<h4 class="subtitle visible-xs">
 											<span><?php echo get_the_excerpt(); ?></span>
 										</h4>
-										<!-- <h4 class="subtitle hidden-xs" style="line-height: 1.5">
-											<span><?php echo get_the_excerpt(); ?></span>
-										</h4> -->
 
 									</div>
 									<div class="pad40 col-sm-12 text-center">
@@ -91,66 +71,52 @@ WP Post Template: Fancy List
 						<!-- The rest of the items -->
 						<?php while( have_rows('items') ): the_row(); ?>
 
-									<?php
-									$title = get_sub_field('title');
-									$subtitle = get_sub_field('subtitle');
-									$pic = get_sub_field('pic');
-									$desc = get_sub_field('desc');
-									?>
+							<?php
+							$title = get_sub_field('title');
+							$subtitle = get_sub_field('subtitle');
+							$pic = get_sub_field('pic');
+							$desc = get_sub_field('desc');
+							?>
 
-									<div class="listItem">
-										<div class="listHero row-fluid" style="background-image: url(<?php echo $pic; ?>)">
-											<div class="listHeader container-fluid">
-												<div class="col-sm-8 col-sm-offset-2 nopad">
-													<?php if($title) : ?>
-														<h1 class="itemTitle">
-															<span>
-																<?php echo $title; ?>
-															</span>
-														</h1>
-													<?php endif; ?>
-
-													<?php if($subtitle) : ?>
-														<h4 class="subtitle">
-															<span><?php echo $subtitle; ?></span>
-														</h4>
-													<?php endif; ?>
-												</div>
-											</div>
-
-											<!-- <div class="listHeroBody visible-xs">
-
-												<?php if($desc) : ?>
-													<h1>
+							<div class="listItem">
+								<div class="listHero row-fluid" style="background-image: url(<?php echo $pic; ?>)">
+									<div class="listHeader container-fluid">
+										<div class="col-sm-8 col-sm-offset-2 nopad">
+											<?php if($title) : ?>
+												<h1 class="itemTitle">
+													<span>
 														<?php echo $title; ?>
-													</h1>
-													<p>
-														<?php echo $desc; ?>
-													</p>
-												<?php endif; ?>
+													</span>
+												</h1>
+											<?php endif; ?>
 
-											</div> -->
-
-										</div>
-
-										<div class="row listBodyContainer">
-											<a id="listBodyToggle" class="listBodyToggle visible-xs"><i class="btr bt-plus"></i></a>
-											<div class="listBody col-sm-8 col-sm-offset-2 nopad">
-												<?php if($desc) : ?>
-													<p>
-														<?php echo $desc; ?>
-													</p>
-												<?php endif; ?>
-											</div>
-
-											<div id="social-links" class="col-sm-8 col-sm-offset-2 nopad">
-												<ul id="post-social" class="post-social hidden-xs hidden-sm">
-													<li><a class="share-facebook" href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
-													<li><a class="share-twitter" href="#" target="popup" onclick="window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
-												</ul>
-											</div>
+											<?php if($subtitle) : ?>
+												<h4 class="subtitle">
+													<span><?php echo $subtitle; ?></span>
+												</h4>
+											<?php endif; ?>
 										</div>
 									</div>
+								</div>
+
+								<div class="row listBodyContainer">
+									<a id="listBodyToggle" class="listBodyToggle visible-xs"><i class="btr bt-plus"></i></a>
+									<div class="listBody col-sm-8 col-sm-offset-2 nopad">
+										<?php if($desc) : ?>
+											<p>
+												<?php echo $desc; ?>
+											</p>
+										<?php endif; ?>
+									</div>
+
+									<div id="social-links" class="col-sm-8 col-sm-offset-2 nopad">
+										<ul id="post-social" class="post-social hidden-xs hidden-sm">
+											<li><a class="share-facebook" href="#" target="popup" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>','Share this post on Facebook','width=600,height=400')"><i class="fa fa-facebook"></i></a></li>
+											<li><a class="share-twitter" href="#" target="popup" onclick="window.open('https://twitter.com/share?url=<?php the_permalink(); ?>','Tweet this post','width=600,height=400')"><i class="fa fa-twitter"></i></a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
 
 						<?php endwhile; // while( has_sub_field('items') ): ?>
 
@@ -159,10 +125,6 @@ WP Post Template: Fancy List
 				<?php endif; // if( get_field('items') ): ?>
 
 				<!-- END ITEMS -->
-
-
-
-				<!-- End Items on List -->
 
 			</article>
 			<?php endwhile; ?>
