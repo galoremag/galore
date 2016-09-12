71ec57f8740b6bae3c5e8f3aa350af0f2c800652
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-09-12 18:11:32 -0400

    tweak sizing of each item

--- js/main.js
+++ js/main.js
@@ -412,7 +412,7 @@ jQuery(document).ready(function($) {
     $.scrollify({
       section: ".listItem",
       sectionName : "section-name",
-      offset: 31,
+      offset: 0,
       // interstitialSection : "header, #footer-open, #ctoolbar, #listBodyToggle",
       standardScrollElements: ".listBodyContainer",
       scrollSpeed: 400
