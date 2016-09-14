c5334908133ef3263c1b8785637819b0149c1109
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-09-13 22:02:23 -0700

    tweak RM

--- single-list.php
+++ single-list.php
@@ -104,7 +104,7 @@ WP Post Template: Fancy List
 												</h4>
 											<?php endif; ?>
 
-											<a href="javascript:void;" class="listReadMore"><h4>Read More</h4></a>
+											<a href="javascript:void;" class="listReadMore">Read More <i class="btr bt-angles-right"></i></a>
 										</div>
 									</div>
 								</div>
