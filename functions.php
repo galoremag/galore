42674f359812c600a260f26ae8db04d5944cc8d7
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-09-29 10:33:48 -0700

    add fancy lists to hero slider

--- functions.php
+++ functions.php
@@ -152,7 +152,7 @@
 
 	    // Query #1 parameters  https://gist.github.com/luetkemj/2023628
 	    $args = array(
-					'post_type' => array('list'),
+				'post_type' => array('list'),
 	        'posts_per_page' => 3,
 	        'orderby' => 'date',
 					'tag' => 'featured'
@@ -428,17 +428,17 @@
 	}
 
 	// Load ad script after Ajax load more item
-	<!-- /60899964/Article_Mobile_300x250_pos2 -->
-<div id='div-gpt-ad-1465835581876-4' style='height:250px; width:300px;' class="visible-xs center-block">
-<script type='text/javascript'>
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-4'); });
-</script>
-</div>
-
-<!-- /60899964/Article_300x250_970x250_pos2 -->
-<div id='div-gpt-ad-1465835581876-1' style='height:250px; width:300px;' class="hidden-xs center-block">
-<script type='text/javascript'>
-googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-1'); });
-</script>
+// 	<!-- /60899964/Article_Mobile_300x250_pos2 -->
+// <div id='div-gpt-ad-1465835581876-4' style='height:250px; width:300px;' class="visible-xs center-block">
+// <script type='text/javascript'>
+// googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-4'); });
+// </script>
+// </div>
+//
+// <!-- /60899964/Article_300x250_970x250_pos2 -->
+// <div id='div-gpt-ad-1465835581876-1' style='height:250px; width:300px;' class="hidden-xs center-block">
+// <script type='text/javascript'>
+// googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-1'); });
+// </script>
 
 ?>
