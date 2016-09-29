6494cc9573875982ef9ec06a952674178d180a0e
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-09-29 15:28:08 -0700

    FINALLY get the fancy lists in the daily dish (only, so far) and then adjust the snapchat native small CSS

--- home.php
+++ home.php
@@ -32,7 +32,8 @@
 					'order' => 'DESC',
 					'orderby' => 'date',
 					'offset' => 0,
-					'post_type' => 'any',
+					'post_type' => array('list', 'post'),
+					'public' => true,
 					'tax_query' => array(
 			      array(
 			        'tax_query' => array(
