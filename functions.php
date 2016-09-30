e6ede25674c2f7ef624b2db04f1faf6c4376ca00
Aaron Smulktis <aaronsmulktis@gmail.com>
2016-09-29 17:16:14 -0700

    try to get fancy list posts into hero slider AGAIN

--- functions.php
+++ functions.php
@@ -145,52 +145,81 @@
 	}
 
 	// MOST RECENT POSTS FROM TAG 'FEATURED'
-	function add_additional_posts_to_slider($slides, $options, $type) {
-	    if( $options['id'] !== 1 ) { return $slides; }
+	// function add_additional_posts_to_slider($slides, $options, $type) {
+	//     if( $options['id'] !== 1 ) { return $slides; }
+	//
+	//     $slides = array();
+	//
+	//     // Query #1 parameters  https://gist.github.com/luetkemj/2023628
+	//     $args = array(
+	// 			'post_type' => array('list'),
+	//       'posts_per_page' => 3,
+	//       'orderby' => 'date',
+	// 			'tag' => 'featured',
+	// 			'order' => 'DESC',
+	// 			'orderby' => 'date',
+	// 			'offset' => 0,
+	// 			'post_type' => array('list', 'post'),
+	// 			'public' => true,
+	// 			'post__not_in' => get_option( 'sticky_posts' ),
+	// 			'tax_query' => array(
+	// 				array(
+	// 					'tax_query' => array(
+	// 						array(
+	// 							'taxonomy' => 'sections', // change taxonomy
+	// 							'field' => 'slug',
+	// 							'terms' => 'hidehome',
+	// 							'operator' => 'NOT IN'
+	// 							)
+	// 						)
+	// 					)
+	// 				)
+	//     );
+	//     $query = new WP_Query($args);
+	//     $slides = array_merge($slides,  (array)$query->posts); // merge queried data
+	//
+	//     // Query #2 parameters
+	//     // $args = array(
+	// 		// 	'post_type' => array('list'),
+	// 		// 	'posts_per_page' => 3,
+	// 		// 	'orderby' => 'date',
+	// 		// 	'tag' => 'featured'
+	//     // );
+	//     // $query = new WP_Query($args);
+	//     // $slides = array_merge($slides, (array)$query->posts); // merge queried data
+	//
+	//     return $slides;
+	// }
+	// add_filter('new_rs_slides_filter', 'add_additional_posts_to_slider', 10, 3);
 
-	    $slides = array();
-
-	    // Query #1 parameters  https://gist.github.com/luetkemj/2023628
-	    $args = array(
-				'post_type' => array('list'),
-	      'posts_per_page' => 3,
-	      'orderby' => 'date',
-				'tag' => 'featured',
-				'order' => 'DESC',
-				'orderby' => 'date',
-				'offset' => 0,
-				'post_type' => array('list', 'post'),
-				'public' => true,
-				'post__not_in' => get_option( 'sticky_posts' ),
-				'tax_query' => array(
-					array(
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'sections', // change taxonomy
-								'field' => 'slug',
-								'terms' => 'hidehome',
-								'operator' => 'NOT IN'
-								)
+	add_filter('new_royalslider_posts_slider_query_args', 'galorers_custom_query', 10, 2);
+	function galorers_custom_query($args) {
+		$args = array(
+			'post_type' => array('list'),
+			'posts_per_page' => 3,
+			'orderby' => 'date',
+			'tag' => 'featured',
+			'order' => 'DESC',
+			'orderby' => 'date',
+			'offset' => 0,
+			'post_type' => array('list', 'post'),
+			'public' => true,
+			'post__not_in' => get_option( 'sticky_posts' ),
+			'tax_query' => array(
+				array(
+					'tax_query' => array(
+						array(
+							'taxonomy' => 'sections', // change taxonomy
+							'field' => 'slug',
+							'terms' => 'hidehome',
+							'operator' => 'NOT IN'
 							)
 						)
 					)
-	    );
-	    $query = new WP_Query($args);
-	    $slides = array_merge($slides,  (array)$query->posts); // merge queried data
-
-	    // Query #2 parameters
-	    // $args = array(
-			// 	'post_type' => array('list'),
-			// 	'posts_per_page' => 3,
-			// 	'orderby' => 'date',
-			// 	'tag' => 'featured'
-	    // );
-	    // $query = new WP_Query($args);
-	    // $slides = array_merge($slides, (array)$query->posts); // merge queried data
-
-	    return $slides;
+				)
+		);
+	  return $args;
 	}
-	add_filter('new_rs_slides_filter', 'add_additional_posts_to_slider', 10, 3);
 
 	/** Add Social Sharing Links on Single Posts **/
 	add_action('genesis_after_entry', 'include_social', 9);
