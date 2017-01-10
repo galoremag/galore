a36e3831a4764191365efae21b533469bffd8fc5
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-01-09 21:54:26 -0800

    make another attempt to fix the PHP syntax error, but its trivial anyway

--- archive.php
+++ archive.php
@@ -73,4 +73,5 @@
 		</div>
 	</div>
 </div>
-<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
+
+<?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer') ); ?>
