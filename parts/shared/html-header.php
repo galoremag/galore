e46c87ee04167c90c5d52c2f819cc5a5cb59e351
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-02-21 18:35:44 -0800

    shift even more DFP code to the plugin

--- parts/shared/html-header.php
+++ parts/shared/html-header.php
@@ -185,22 +185,6 @@
 		</noscript>
 		<!-- End Quantcast Tag -->
 
-		<!-- DFP script -->
-		<script type='text/javascript'>
-		  var googletag = googletag || {};
-		  googletag.cmd = googletag.cmd || [];
-		  (function() {
-		    var gads = document.createElement('script');
-		    gads.async = true;
-		    gads.type = 'text/javascript';
-		    var useSSL = 'https:' == document.location.protocol;
-		    gads.src = (useSSL ? 'https:' : 'http:') +
-		      '//www.googletagservices.com/tag/js/gpt.js';
-		    var node = document.getElementsByTagName('script')[0];
-		    node.parentNode.insertBefore(gads, node);
-		  })();
-		</script>
-
 		<!-- DFP header scripts moved to Ad Switcher plugin -->
 
 		<!-- reCAPTCHA forms -->
