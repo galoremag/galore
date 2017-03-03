a06843a9a3b1114e7e40ea3f70c683bc6806b95e
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-03-02 17:36:09 -0800

    add wildcard to allowed origin

--- functions.php
+++ functions.php
@@ -429,7 +429,7 @@
 	add_action( 'rest_api_init', function() {
 		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
 		add_filter( 'rest_pre_serve_request', function( $value ) {
-			header( 'Access-Control-Allow-Origin: http://165.225.129.212' );
+			header( 'Access-Control-Allow-Origin: *' );
 			header( 'Access-Control-Allow-Methods: GET' );
 			header( 'Access-Control-Allow-Credentials: true' );
 
