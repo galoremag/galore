0b84ba952a4569c55c980980f8b5d5d542014c0c
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-03-02 17:32:02 -0800

    update WP-API to include newly needed CORS (cross origin request service) for only our youtube scraper IP through functions.php

--- functions.php
+++ functions.php
@@ -425,6 +425,19 @@
 		// return $size_data;
 	}
 
+	// Add CORS to WP-API
+	add_action( 'rest_api_init', function() {
+		remove_filter( 'rest_pre_serve_request', 'rest_send_cors_headers' );
+		add_filter( 'rest_pre_serve_request', function( $value ) {
+			header( 'Access-Control-Allow-Origin: http://165.225.129.212' );
+			header( 'Access-Control-Allow-Methods: GET' );
+			header( 'Access-Control-Allow-Credentials: true' );
+
+			return $value;
+			
+		});
+	}, 15 );
+
 	add_theme_support( 'title-tag' );
 	// add_theme_support('auto-load-next-post');
 
