8f77318d51b87fa6bd35ad440986ff434afdbf2b
Aaron Smulktis <aaronsmulktis@gmail.com>
2017-08-30 13:14:25 -0700

    remove unused images, start updating About page template

--- page-about.php
+++ page-about.php
@@ -17,138 +17,101 @@ Template Name: About
 <div class="container-fluid">
 	<div class="row-fluid">
 		<div id="content" class="col-sm-10 col-sm-offset-1">
-			<div class="row-fluid">
+			<div class="row">
 				<div class="col-sm-12">
-					<h1 class="text-center"><span>Who We</span> Are</h1>
-					<p class="section__intro">Galore Media reaching the Fun, Fearless and Fashionable girl through a mix of digital and social media, print and creative services. Galore is also a creative and marketing agency that brings a unique point of view, awesome talent relations, and creative direction to both develop fresh new work, and extend existing client campaigns through social media.</p>
+					<h1 class="text-center">A <span>Talent-Driven</span> Content Studio</h1>
+					<p class="section__intro">Galore Media is the voice for the cool, creative and daring Gen Z girl. We work with brands across all categories to create campaigns that work across all mobile platforms, driven by talent and premium creative. Along with it's own channels, Galore Media is able to drive significant reach through it's influencer platform Kitten, and through an expertise in paid amplification for Instagram and Facebook.</p>
 				</div>
 			</div>
 			<section id="ad-campaigns">
-				<div class="row-fluid">
-					<div class="col-sm-12">
-						<h2 class="align--center">Ad Campaigns</h2>
-						<p class="section__intro">As a creative services agency, Galore works directly with clients to concept and create ad campaigns in print, digital and video. This includes high level thinking surrounding brand ID and concept, to all elements of production such as casting, location scouting, and of course, photography and video. All with the signature Galore style!</p>
-					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_fredericks.jpg" alt="Fredericks of Hollywood">
-					</div>
-					<div class="col-sm-8">
-						<h3>Fredericks of Hollywood</h3>
-						<p>Galore Loves Fredericks! As part of an ongoing collaboration which has included events, print and video, Galore and Fredericks took their partnership to next level when Fredericks enlisted Galore to creative direct, cast, and photograph their Fall 2013 catalogue. Shot over a 3-day period, the Fall Catalogue promises to be the sexiest one of the year.</p>
-					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-8">
-						<h3>Uberliss</h3>
-						<p>A new premium hair straightening system Uberliss is an industry leader in advanced haircare technology. In order to break into this competitive marketplace Uberliss tapped Galore to develop their brand campaign, responsible for the creative direction and photography for the campaign. Using its media intelligence and industry relationships Galore casted a model with a large social media presence to aid in raising the Uberliss brand awareness. Additional social media overlays and events via Galore have elevated Uberliss into a recognized cool brand with product results to match. </p>
-					</div>
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_uberliss.jpg" alt="Uberliss">
-					</div>
-				</div><!-- .row-fluid -->
-			</section>
-			<section id="video-series">
-				<div class="row-fluid">
-					<div class="col-sm-12">
-						<h2 class="align--center">Video Series</h2>
-						<p class="section__intro">Galore has two divisions focused on video, Galore Cinema and Galore TV. Galore TV is short form video, featuring the different personalities of the Galore wold. Galore Cinema is focused on long form video, both scripted and un-scripted, that brings the Galore vision to life with blockbuster quality.</p>
-					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid pad--bottom">
-					<div class="col-sm-12">
-						<iframe width="420" height="315" src="//www.youtube.com/embed/iO1HJDU7HJY" frameborder="0" allowfullscreen></iframe>
-					</div>
-				</div>
 				<div class="row">
-					<div class="col-sm-8">
-						<h3>Steve Madden</h3>
-						<p>Galore worked with Steve Madden on a custom video series surrounding New York Fashion Week. Serving as creative directors and producers, Galore enlisted fun fashion personalities like Andre J and Lysa Cooper, model Andrej Pejic and it girl Liza Thorn to serve as Steve Madden and Galore on-camera ambassadors, bringing a different type of perspective to Fashion Week. The video lived on Galoremag.com and on Youtube, combined reaching over 100K viewers.</p>
+					<div class="col-sm-10 col-sm-offset-1 text-center">
+						<h3 class="align--center">Snapshot <i class="fa fa-globe"></i></h3>
+						<ul class="text-left">
+							<li>+ 25M videos views across our channels</li>
+							<li>+ Featured talent including Kylie Jenner, Bella Thorne, Zendaya, Gigi Hadid and many others.</li>
+							<li>+ 100 pieces of Branded content per month</li>
+							<li>+ Representing over 150 top creators</li>
+							<li>+ Includes talent including Ava Allen, Ava Dash, Nala Wayans and more</li>
+							<li>+ 60M daily reach</li>
+							<li>+ Over 100M mobile video views each month</li>
+						</ul>
 					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid pad--bottom">
-					<div class="col-sm-12">
-						<iframe src="//player.vimeo.com/video/77536706" width="500" height="286" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
-					</div>
-				</div>
+				</div><!-- .row -->
+			</section>
+
+			<section id="iphone-sim">
 				<div class="row">
-					<div class="col-sm-8">
-						<h3>Run. Catch. Kill. A Galore Cinema Exclusive</h3>
-						<p>Galore Media is proud to announce the premiere of its first short film Run. Catch. Kill., a short film starring models Ashley Smith and Mia Tyler, written and directed by Joey Falsetta. The film takes place in rural Vermont where a teenage girl (played by Ashley Smith) is haunted by a desire to stay young and beautiful and must fulfill an evil pact made long ago with a coven of supernatural beauties (lead by Mia Tyler’s character). In the end she must make a decision between capturing everlasting beauty or sparing the lives of her loved ones.</p>
-					</div>
-				</div>
-			</section>
-			<section id="social-promotions">
-				<div class="row-fluid">
 					<div class="col-sm-12">
-						<h2 class="align--center">Social + Digital Promotions</h2>
-						<p class="section__intro">Galore has a network of 25 model and “it girl” bloggers that serve to amplify digital campaigns. These Galore Girls include Models Ashley Sky, Musician Jennifer Akerman, Model and Food Blogger’s Robyn Lawley and Elizabeth Minnett, and many more. The combined social reach exceeds 10MM followers through social media channels.</p>
+						<div id="ava-video">
+							<video></video>
+						</div>
+						<p class="caption text-">Ava Allan for Bongo</p>
+						<p>We make videos for where our audience of Gen Z girls lives, chiefly Instagram and Snap. We shoot daily from our LA and NY studios, powering the social channels of our partners with on-brand, talent lead video content.</p>
 					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-4">
-						<p class="stat">25<small>Model and It Girls</small></p>
-					</div>
-					<div class="col-sm-4">
-						<p class="stat">10M<small>Combined Social Media Followers</small></p>
-					</div>
-					<div class="col-sm-4">
-						<p class="stat">15.5M<small>Total Network Audience</small></p>
-					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_neuro.jpg" alt="Neuro BLISS">
-					</div>
-					<div class="col-sm-8">
-						<h3>Neuro BLISS</h3>
-						<p>Galore and Neuro worked together on two summer parties, one in NYC and the other in Miami during SWIM. In addition to providing great signage, and sampling opportunities, Galore extended the campaign in real-time through our Galore Girls network reaching over 1.5MM through instagram alone.</p>
-					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-8">
-						<h3>Bebe #be9to5</h3>
-						<p>Leading up to the unveiling of Bebe’s fall Ad campaign Galore was retained to use its media assets and network of Galore Girls (influencer models and It girls) to populate the #be9to5 microsite. Components of the program included editorial driven content via Galore social media, elevated and aspirational content via Galore’s network, and media exposure via Galore digital. Combined impressions totaled over 600k.</p>
-					</div>
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_bebe.jpg" alt="Bebe #be9to5">
-					</div>
-				</div>
+				</div><!-- .row -->
 			</section>
-			<section id="custom-publishing">
-				<div class="row-fluid">
+
+			<section id="ad-campaigns">
+				<div class="row">
 					<div class="col-sm-12">
-						<h2 class="align--center">Custom Publishing</h2>
+						<h2 class="align--center">Global Talent Campaigns</h2>
+						<img src="" title="Sahara Ray and Sofia Jamora packaging for L'Oreal" />
+						<p>Five top influencers were used for L'Oreal's Global Colorista Campaign</p>
+						<p>When L'Oreal did it's first global campaign with influencers, they came to our agency Kitten, who represents over 150 of the biggest young stars on social media. The Colorista campaign included original video, a multi-day shoot in Paris, and being including on the product packaging.</p>
 					</div>
-				</div>
-				<div class="row-fluid">
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_steve-madden.jpg" alt="Steve Madden">
+				</div><!-- .row -->
+			</section>
+
+			<section id="ad-campaigns">
+				<div class="row">
+					<div class="col-sm-12">
+						<h3 class="align--center">Trend and Consumer Insights</h3>
+						<img src="" title="???" />
+						<p>Galore Media has a proprietary network of 10K culture-makers, who have been identified to have a forward looking view on "what's next" in culture, and inform our research and trend forecasting product.</p>
+						<h2>HOW IT WORKS</h2>
+						<ul>
+							<li>• Rapid polling to specific demos</li>
+							<li>• Social listening on given topic</li>
+							<li>• Galore Trend Insights team to collapse data into a custom report</li>
+							<li>• For product releases, can leverage data with qualitative to provide a confidence score for a specific launch</li>
+							<li>• Film or TV releases will also receive an affinity score from Culture Makers within this segment.</li>
+						</ul>
 					</div>
-					<div class="col-sm-8">
-						<h3>Steve Madden</h3>
-						<p>Steve Madden approached Galore to do a custom publishing takeover of their own Madden Mag. Galore played guest editor and publisher fully orchestrating fashion photo shoots, creative direction, and artwork. The partnership was then celebrated by a Galore produced buzzed event in the rooftop lounge of the James Hotel NYC, The Jimmy.</p>
+				</div><!-- .row -->
+			</section>
+
+			<section id="influecer-promotions">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2 class="align--center">Influencer promotions</h2>
+						<img src="" title="???" />
+						<h4>INFLUENCER for BRAND</h4>
+						<p>The Kitten roster is filled with some of the best creators for Instagram and Snap. Best of all, they have huge audiences that allow them to drive awareness for a brand across their networks, with very high engagement.</p>
 					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-8">
-						<h3>The Fancy</h3>
-						<p>Galore and The Fancy share a love of great pictures. And what better way to showcase the Fancy brand then their own custom photo magazine. Shot over 5 days, Galore casted and shot a series of looks for TheFancy.com, which culminated in a custom magazine, just in time for New York Fashion Week.</p>
+				</div><!-- .row -->
+			</section>
+
+			<section id="influecer-promotions">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2 class="align--center">Product Collaborations</h2>
+						<img src="" title="Missguided" />
+						<p>When it comes to product collaborations, Galore's fans turn up. Working with Missguided, the Galore creative team designed a 20-piece collection inspired by Lil' Kim, and shot and produced all of the creative assets including the lookbook, bts video, a kit of social assets, in addition to getting the clothes on celebrities like Paris Hilton and Keke Palmer, and pushed out to our Kitten influencers and Galore Girls. Result....the collection sold out in 2 Weeks.</p>
 					</div>
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_fancy.jpg" alt="The Fancy">
+				</div><!-- .row -->
+			</section>
+
+			<section id="influecer-promotions">
+				<div class="row">
+					<div class="col-sm-12">
+						<h2 class="align--center">Experiential</h2>
+						<img src="" title="Party Photos" />
+						<p>With Instagram turning events into media moments (and the pull of Netflix keeping people at home), unique experiential moments are more important than ever. Either through music driven events like Galore Fashion Week, Miami SWIM, Sisterhood of Sound, or our new music Festival Girl Cult, Galore brings a creative and fun approach to live events, which are always attended by the most sought after names in our target demo.</p>
+						<!-- https://galoremag.com/kylie-jenner-hosted-our-generation-bombshell-issue-release-party-check-it-out/
+						https://galoremag.com/highlights-from-last-nights-girl-cult-dinner-with-keke-palmer/ -->
 					</div>
-				</div><!-- .row-fluid -->
-				<div class="row-fluid">
-					<div class="col-sm-4">
-						<img src="<?php bloginfo('template_url'); ?>/images/about/marketing_hospitality.jpg" alt="Hospitality">
-					</div>
-					<div class="col-sm-8">
-						<h3>Hospitality</h3>
-						<p>Welcome to the party. Galore’s cult following of It girls, scenesters, models, and creatives are a highly sought after demographic. For our hospitality partners Galore brings our audience to life by way of consumer promotions, PR driven events, programing curation, and cross-promotional opportunities.</p>
-					</div>
-				</div><!-- .row-fluid -->
+				</div><!-- .row -->
 			</section>
 		</div><!-- #content -->
 	</div>
