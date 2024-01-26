ddc9b34fb62f3063fb4c2b257e5ed68a815d8bf3
Aaron Smulktis <aaronsmulktis@gmail.com>
2024-01-26 13:51:48 -0500

    tweak header

--- wp-content/themes/galore/parts/shared/header.php
+++ wp-content/themes/galore/parts/shared/header.php
@@ -1,52 +1,38 @@
 <!-- LEADERBOARD -->
 <?php if ( is_single() ) : ?>
 
-	<?php if ( wp_is_mobile() ) : ?>
-		<!-- /60899964/Article_Mobile_Leaderboard -->
-		<div id='div-gpt-ad-1465835581876-6' class="center-block text-center">
-		<script type='text/javascript'>
-		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-6'); });
-		</script>
-		</div>
-	<?php endif; ?>
+<?php if ( wp_is_mobile() ) : ?>
+	<!-- /60899964/Article_Mobile_Leaderboard -->
+	<div id='div-gpt-ad-1465835581876-6' class="center-block text-center">
+	<script type='text/javascript'>
+	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-6'); });
+	</script>
+	</div>
+<?php endif; ?>
 
 <?php else : ?>
 
-	<?php if ( wp_is_mobile() ) : ?>
-		<!-- /60899964/Home_Mobile_Leaderboard -->
-		<div id='div-gpt-ad-1465835581876-17' class="center-block text-center">
-		<script type='text/javascript'>
-		googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-17'); });
-		</script>
-		</div>
-	<?php endif; ?>
+<?php if ( wp_is_mobile() ) : ?>
+	<!-- /60899964/Home_Mobile_Leaderboard -->
+	<div id='div-gpt-ad-1465835581876-17' class="center-block text-center">
+	<script type='text/javascript'>
+	googletag.cmd.push(function() { googletag.display('div-gpt-ad-1465835581876-17'); });
+	</script>
+	</div>
+<?php endif; ?>
 
 <?php endif; ?>
 
 <?php if ( wp_is_mobile() ) : ?>
 
-	<header itemscope="itemscope" itemtype="http://schema.org/WPHeader" role="banner">
-		<div class="container-fluid nopad">
-			<!-- Fixed navbar -->
-			<nav class="navbar navbar-default shrink" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<div id="modal-nav-button" class="nav-button pull-left">
-							<div id="nav-button"><a href="#" id="hmbrgr" class="hmbrgr"></a></div>
-						</div>
-						<div id="site-search" class="pull-left hidden-xs">
-							<!-- <?php get_search_form(); ?> -->
-							<form role="search" method="get" action="<?php echo site_url(); ?>">
-								<label class="form-group">
-									<input type="search" class="form-control" placeholder="Search" value="Search" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-								</label>
-								<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-							</form>
-						</div>
-						<!-- <button id="modal-nav-button" type="button" class="nav-button">
-						<i class="fa fa-bars"></i>
-						</button> -->
-						<a id="navbar-brand" class="navbar-brand" href="<?php bloginfo('url'); ?>"></a>
+<header itemscope="itemscope" itemtype="http://schema.org/WPHeader" role="banner">
+	<div class="container-fluid nopad">
+		<!-- Fixed navbar -->
+		<nav class="navbar navbar-default shrink" itemscope="itemscope" itemtype="http://schema.org/SiteNavigationElement" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<div id="modal-nav-button" class="nav-button pull-left">
+						<div id="nav-button"><a href="#" id="hmbrgr" class="hmbrgr"></a></div>
 					</div>
 					<div>
 						<ul class="top-social navbar-right tab hidden-xs">
@@ -81,11 +67,15 @@
 					<div id="site-search" class="visible-xs">
 						<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 							<label class="form-group">
-								<input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+								<input type="search" class="form-control" placeholder="Search" value="Search" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 							</label>
 							<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 						</form>
 					</div>
+					<!-- <button id="modal-nav-button" type="button" class="nav-button">
+					<i class="fa fa-bars"></i>
+					</button> -->
+					<a id="navbar-brand" class="navbar-brand" href="<?php bloginfo('url'); ?>"></a>
 				</div>
 				<div>
 					<ul class="top-social navbar-right tab hidden-xs">
@@ -101,33 +91,57 @@
 					</ul>
 				</div><!--/.nav-collapse -->
 			</div>
+		</nav>
+	</div>
+
+	<div id="main-menu" class="modal-nav">
+		<div class="modal-nav-content">
+			<ul class="modal-nav-menu text-center">
+				<li><a href="http://tv.galoremag.com" title="GaloreTV" target="_blank"><i class="fa fa-television"></i> GaloreTV</li>
+				<li><a href="<?php bloginfo('url'); ?>/category/girl-cult" title="Girl Cult">Girl Cult <span class="badge">New</span></a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a> + <a href="<?php bloginfo('url'); ?>/category/fashion" title="Fashion">Fashion</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/dating" title="Dating">Dating</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/health" title="health">Health</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a> + <a href="<?php bloginfo('url'); ?>/category/politics" title="Politics">Politics <span class="badge">New</span></a></li>
+<!-- 					<hr />
+				<h1>Join the movement</h1>
+				<li class="span"><a href="https://galoremag.com/contribute" title="Contribute" target="_blank">Become a Contributor</a> and <a href="https://galoremag.com/galore-girl" title="GaloreGirl" target="_blank">Apply to become a Galore Girl</a></li>
+				<div id="site-search" class="visible-xs">
+					<!- <?php get_search_form(); ?> ->
+					<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+						<label class="form-group">
+							<input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+						</label>
+						<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+					</form>
+				</div> -->
+			</div>
+			<div>
+				<ul class="top-social navbar-right tab hidden-xs">
+					<li><a href="<?php bloginfo('url'); ?>/category/girl-cult" title="Girl Cult" target="_blank">Girl Cult</a></li>
+<!-- 						<li><a id="signupButton" href="#"><i class="fa fa-envelope"></i></a></li> -->
+					<li><a href="https://www.facebook.com/galore" target="_blank"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="http://instagram.com/galore/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+					<li><a href="https://twitter.com/thegaloremag" target="_blank"><i class="fa fa-twitter"></i></a></li>
+					<!-- <li><a href="http://princeandjacob.tumblr.com/" target="_blank"><i class="fa fa-tumblr"></i></a></li> -->
+					<!-- <li><a href="https://www.youtube.com/channel/UCyzzsgpNlmLBKYcXLM3Ro3g" target="_blank"><i class="fa fa-youtube-play"></i></a></li> -->
+					<!-- <li class=""><a href="http://www.slashergirl.com/collections/shop-galore?ref=galore" target="_blank">SHOP</a></li> -->
+				</ul>
+			</div><!--/.nav-collapse -->
 		</div>
-	</header>
+	</div>
+</header>
 
 <?php else : ?>
 
-	<header>
-		<div class="container-fluid nopad">
-			<!-- Fixed navbar -->
-			<nav id="deskNav" class="navbar navbar-default navbar-fixed-top" role="navigation">
-				<div class="container-fluid">
-					<div class="navbar-header">
-						<div id="modal-nav-button" class="nav-button pull-left">
-							<div id="nav-button"><a href="#" id="hmbrgr" class="hmbrgr"></a></div>
-						</div>
-						<div id="site-search" class="pull-left hidden-xs">
-							<!-- <?php get_search_form(); ?> -->
-							<form role="search" method="get" action="<?php echo site_url(); ?>">
-								<label class="form-group">
-									<input type="search" class="form-control" placeholder="Search" value="Search" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
-								</label>
-								<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
-							</form>
-						</div>
-						<!-- <button id="modal-nav-button" type="button" class="nav-button">
-						<i class="fa fa-bars"></i>
-						</button> -->
-						<a id="navbar-brand" class="navbar-brand" href="<?php bloginfo('url'); ?>"></a>
+<header>
+	<div class="container-fluid nopad">
+		<!-- Fixed navbar -->
+		<nav id="deskNav" class="navbar navbar-default navbar-fixed-top" role="navigation">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<div id="modal-nav-button" class="nav-button pull-left">
+						<div id="nav-button"><a href="#" id="hmbrgr" class="hmbrgr"></a></div>
 					</div>
 					<div>
 						<ul class="top-social navbar-right tab hidden-xs">
@@ -167,15 +181,62 @@
 					<div id="site-search" class="visible-xs">
 						<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 							<label class="form-group">
-								<input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+								<input type="search" class="form-control" placeholder="Search" value="Search" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 							</label>
 							<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 						</form>
 					</div>
-				</ul>
+					<button id="modal-nav-button" type="button" class="nav-button">
+					<i class="fa fa-bars"></i>
+					</button> -->
+					<a id="navbar-brand" class="navbar-brand" href="<?php bloginfo('url'); ?>"></a>
+				</div>
+				<div>
+					<ul class="top-social navbar-right tab hidden-xs">
+						<li><a href="<?php bloginfo('url'); ?>/category/girl-cult" title="Girl Cult" target="_blank">Girl Cult</a></li>
+ 						<!-- <li><a id="signupButton" href="#"><i class="fa fa-envelope"></i></a></li> -->
+						<li><a href="https://www.facebook.com/galore" target="_blank"><i class="fa fa-facebook"></i></a></li>
+						<li><a href="http://instagram.com/galore/" target="_blank"><i class="fa fa-instagram"></i></a></li>
+						<li><a href="https://twitter.com/thegaloremag" target="_blank"><i class="fa fa-twitter"></i></a></li>
+						<!-- <li><a href="http://princeandjacob.tumblr.com/" target="_blank"><i class="fa fa-tumblr"></i></a></li> -->
+						<!-- <li><a href="https://www.youtube.com/channel/UCyzzsgpNlmLBKYcXLM3Ro3g" target="_blank"><i class="fa fa-youtube-play"></i></a></li> -->
+						<!-- <li class=""><a href="http://www.slashergirl.com/collections/shop-galore?ref=galore" target="_blank">SHOP</a></li> -->
+					</ul>
+				</div><!--/.nav-collapse -->
 			</div>
+		</nav>
+	</div>
+
+	<div id="main-menu" class="modal-nav">
+		<div class="modal-nav-content">
+			<!-- <div class="modal-nav-header">
+				<div class="modal-nav-close">
+					<i class="btr bt-times"></i>
+				</div>
+			</div> -->
+			<ul class="modal-nav-menu text-center">
+				<li><a href="http://tv.galoremag.com" title="GaloreTV" target="_blank"><i class="fa fa-television"></i> GaloreTV</li>
+				<li><a href="<?php bloginfo('url'); ?>/category/girl-cult" title="Girl Cult">Girl Cult <span class="badge">New</span></a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a> + <a href="<?php bloginfo('url'); ?>/category/fashion" title="Fashion">Fashion</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/dating" title="Dating">Dating</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/health" title="health">Health</a></li>
+				<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a> + <a href="<?php bloginfo('url'); ?>/category/politics" title="Politics">Politics <span class="badge">New</span></a></li>
+<!-- 					<hr />
+				<h1>Join the movement</h1>
+				<li class="span"><a href="https://galoremag.com/contribute" title="Contribute" target="_blank">Become a Contributor</a> and <a href="https://galoremag.com/galore-girl" title="GaloreGirl" target="_blank">Apply to become a Galore Girl</a></li>
+				<div id="site-search" class="visible-xs">
+					<!- <?php get_search_form(); ?> ->
+					<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
+						<label class="form-group">
+							<input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
+						</label>
+						<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
+					</form>
+				</div> -->
+			</ul>
 		</div>
-	</header>
+	</div>
+</header>
 
 <?php endif; ?>
 
@@ -183,11 +244,11 @@
 
 <?php if ( wp_is_mobile() ) : ?>
 
-	<?php lg_unit(); ?>
+<?php lg_unit(); ?>
 
 <?php endif; ?>
 
 <!-- End Superhero -->
 
 <div itemscope itemtype="http://schema.org/Article" id="global-container" <?php body_class(); ?>>
-	<div id="global-inner" <?php body_class(); ?>>
+<div id="global-inner" <?php body_class(); ?>>
