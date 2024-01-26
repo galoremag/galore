7daee992a8a7fe8d9cf4bd143c0b8fe62f960820
Aaron Smulktis <aaronsmulktis@gmail.com>
2024-01-26 02:41:39 -0500

    fix menus

--- wp-content/themes/galore/parts/shared/header.php
+++ wp-content/themes/galore/parts/shared/header.php
@@ -69,22 +69,23 @@
 			<div class="modal-nav-content">
 				<ul class="modal-nav-menu text-center">
 					<!-- <li><a href="http://tv.galoremag.com" title="GaloreTV" target="_blank"><i class="fa fa-television"></i>GaloreTV <span class="badge">New</span></a></li> -->
-					<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a> + <a href="<?php bloginfo('url'); ?>/category/fashion" title="Fashion">Fashion</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/fashion" title="Fashion">Fashion</a></li>
 					<li><a href="<?php bloginfo('url'); ?>/category/dating" title="Dating">Dating</a></li>
 					<li><a href="<?php bloginfo('url'); ?>/category/health" title="health">Health</a></li>
-					<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a> + <a href="<?php bloginfo('url'); ?>/category/politics" title="Politics">Politics</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/politics" title="Politics">Politics</a></li>
 					<hr />
-					<p>Apply to be a:</p>
+					<!-- <p>Apply to be a:</p>
 					<li><a href="https://galoremag.com/galore-girl" title="GaloreGirl" target="_blank">Galore Girl</a> or <a href="https://galoremag.com/contribute" title="Contribute" target="_blank">Contributor</a></li>
 					<div id="site-search" class="visible-xs">
-						<!-- <?php get_search_form(); ?> -->
 						<form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
 							<label class="form-group">
 								<input type="search" class="form-control" placeholder="<?php echo esc_attr_x( 'Search...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>" />
 							</label>
 							<input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>" />
 						</form>
-					</div>
+					</div> -->
 				</div>
 				<div>
 					<ul class="top-social navbar-right tab hidden-xs">
@@ -154,10 +155,12 @@
 				</div> -->
 				<ul class="modal-nav-menu text-center">
 					<!-- <li><a href="http://tv.galoremag.com" title="GaloreTV" target="_blank"><i class="fa fa-television"></i>GaloreTV <span class="badge">New</span></a></li> -->
-					<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a> + <a href="<?php bloginfo('url'); ?>/category/fashion" title="Fashion">Fashion</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/beauty" title="Beauty">Beauty</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/fashion" title="Fashion">Fashion</a></li>
 					<li><a href="<?php bloginfo('url'); ?>/category/dating" title="Dating">Dating</a></li>
 					<li><a href="<?php bloginfo('url'); ?>/category/health" title="health">Health</a></li>
-					<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a> + <a href="<?php bloginfo('url'); ?>/category/politics" title="Politics">Politics</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/pop" title="Pop">Pop</a></li>
+					<li><a href="<?php bloginfo('url'); ?>/category/politics" title="Politics">Politics</a></li>
 					<!-- <hr />
 					<p>Apply to be a:</p>
 					<li><a href="https://galoremag.com/galore-girl" title="GaloreGirl" target="_blank">Galore Girl</a> or <a href="https://galoremag.com/contribute" title="Contribute" target="_blank">Contributor</a></li>
